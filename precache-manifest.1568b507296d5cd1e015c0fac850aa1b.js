03862aba0bcbd3ad27634d93d6b150aa541c3471
Fernando Almeida <fernando.almeida.pinto@gmail.com>
2020-06-23 02:22:06 -0300

    Updates

---
+++ precache-manifest.1568b507296d5cd1e015c0fac850aa1b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da12db94506fd112ed525ada3e67ee28",
+    "url": "/SimoneMartins/index.html"
+  },
+  {
+    "revision": "22e6edaf0148d8bdfd10",
+    "url": "/SimoneMartins/static/css/2.44531351.chunk.css"
+  },
+  {
+    "revision": "725708f58c6de2268b4f",
+    "url": "/SimoneMartins/static/css/main.a430623b.chunk.css"
+  },
+  {
+    "revision": "22e6edaf0148d8bdfd10",
+    "url": "/SimoneMartins/static/js/2.74a5e024.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SimoneMartins/static/js/2.74a5e024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725708f58c6de2268b4f",
+    "url": "/SimoneMartins/static/js/main.e7f47bbe.chunk.js"
+  },
+  {
+    "revision": "6b83f0f3cb715f3f2234",
+    "url": "/SimoneMartins/static/js/runtime-main.385347aa.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/SimoneMartins/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/SimoneMartins/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/SimoneMartins/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/SimoneMartins/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/SimoneMartins/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "32c5bcf9f29f2dd0a5e4caf61b1ef356",
+    "url": "/SimoneMartins/static/media/main.32c5bcf9.jpg"
+  },
+  {
+    "revision": "776437b1fd4428f83659beb78a6aa4aa",
+    "url": "/SimoneMartins/static/media/tree.776437b1.png"
+  }
+]);
